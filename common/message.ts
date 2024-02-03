80f45d08e4e4acfc8cd5b9c88f1c3c92b43c1341
m4rmen <90349436+m4rmen@users.noreply.github.com>
2024-02-03 16:29:54 -0500

    Cleanup + garden and user interfaces

--- common/message.ts
+++ common/message.ts
@@ -2,3 +2,22 @@ export interface Message {
     title: string;
     body: string;
 }
+
+export interface Garden {
+    id: number;
+    name: string;
+    location: string;
+    photo: string;
+    dimension: string;
+    products: string[];
+}
+
+
+export interface User{
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    gardens: Garden[];
+    points: number;
+}
