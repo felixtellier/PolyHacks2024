d1a53b40590ae872310c51c82dbb2b385e2aff3b
Félix Tellier <felix.tellier@icloud.com>
2024-02-03 17:27:50 -0500

    Add garden-container

--- client/src/app/components/garden-container/garden-container/garden-container.component.ts
+++ client/src/app/components/garden-container/garden-container/garden-container.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Garden } from '@common/garden';
 
 @Component({
     selector: 'app-garden-container',
     templateUrl: './garden-container.component.html',
     styleUrls: ['./garden-container.component.scss'],
 })
-export class GardenContainerComponent {}
+export class GardenContainerComponent implements OnInit {
+    gardens: Garden[];
+
+    ngOnInit(): void {
+        this.gardens = [
+            {
+                id: 1,
+                name: 'Test',
+                location: '2024 rue de bucarest',
+                photo: '1',
+                dimension: '40',
+                products: ['carrot', 'patate'],
+            },
+        ];
+    }
+}
