f036fc12d7ef710c78ca5d3ff24419c56b2ffbff
Félix Tellier <felix.tellier@icloud.com>
2024-02-03 17:13:01 -0500

    First draft for garden-container

--- client/src/app/components/garden-container/garden-container/garden-container.component.ts
+++ client/src/app/components/garden-container/garden-container/garden-container.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-garden-container',
-  templateUrl: './garden-container.component.html',
-  styleUrls: ['./garden-container.component.scss']
+    selector: 'app-garden-container',
+    templateUrl: './garden-container.component.html',
+    styleUrls: ['./garden-container.component.scss'],
 })
-export class GardenContainerComponent {
-
-}
+export class GardenContainerComponent {}
