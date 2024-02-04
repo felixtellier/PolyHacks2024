ca2d6cdb85cc6de4a7a93835366eab80bba86c60
David Anastacio <david.anastacio@polymtl.ca>
2024-02-03 19:53:48 -0500

    Adding garden to mongodb

--- server/app/controller/garden-controller.ts
+++ server/app/controller/garden-controller.ts
@@ -1,4 +1,5 @@
 import { GardenService } from '@app/services/garden-service';
+import { Garden } from '@common/garden';
 import { Request, Response, Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { Service } from 'typedi';
@@ -18,6 +19,16 @@ export class GardenController {
                 const gardens = await this.gardenService.getAllGarden();
 
                 res.status(StatusCodes.OK).json(gardens);
+            } catch (error) {
+                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+            }
+        });
+
+        this.router.put('/', async (req: Request, res: Response) => {
+            try {
+                const garden: Garden = req.body;
+                await this.gardenService.addGarden(garden);
+                res.status(StatusCodes.CREATED).send();
             } catch (error) {
                 res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
             }
