234ff3daf1ff2d141cd5cc2c3ec748d78a98554a
Félix Tellier <felix.tellier@icloud.com>
2024-02-04 07:49:32 -0500

    Add function to send bag

--- client/src/app/components/confirmation-dialog/confirmation-dialog.component.ts
+++ client/src/app/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -8,5 +8,11 @@ import { Garden } from '@common/garden';
     styleUrls: ['./confirmation-dialog.component.scss'],
 })
 export class ConfirmationDialogComponent {
+    numberOfBag = 1;
     constructor(@Inject(MAT_DIALOG_DATA) public garden: Garden) {}
+
+    sendBag() {
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        if (this.numberOfBag < 0 || this.numberOfBag > 31) return;
+    }
 }
