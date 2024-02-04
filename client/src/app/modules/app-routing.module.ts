c8b795faf8a92b581922882f27bc02dcc0834a85
m4rmen <90349436+m4rmen@users.noreply.github.com>
2024-02-03 20:46:06 -0500

    merge and topbar updates, profile page

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { GardenCreationComponent } from '@app/pages/garden-creation/garden-creation.component';
 import { InfoPageComponent } from '@app/pages/info-page/info-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
+import { ProfilePageComponent } from '@app/pages/profile-page/profile-page.component';
 
 const routes: Routes = [
     { path: 'home', component: MainPageComponent },
     { path: 'garden-creation', component: GardenCreationComponent },
     { path: 'info', component: InfoPageComponent },
+    { path: 'profile-page', component: ProfilePageComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/home' },
 ];
